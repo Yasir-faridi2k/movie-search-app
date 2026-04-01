c60dff59e5fee88fc43aae3558dc0f18dc8129f4
Md Yasir Faridi <mdyasirfaridi@gmail.com>
2026-04-01 23:48:20 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,3 @@
-// src/components/MovieCard.js - Single movie tile shown in search results
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFavorites } from '../context/FavoritesContext';
